58f667d38d64aa5572a8342e52b1c2769a238c1f
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-02-01 16:26:58 +0530

    UC 5 – Display All Employee Payroll Details from JSON Object

--- home.js
+++ home.js
@@ -11,20 +11,67 @@ const createInnerHtml = () => {
         "<th>Salary</th>" +
         "<th>Start Date</th>" +
         "<th>Actions</th>";
-    const innerHtml = `${headerHtml}
+    let innerHTML = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList) {
+        innerHTML = `${innerHTML}
         <tr>
-            <td><img class="profile" src="Media/emp1.png" alt=""></td>
-            <td>Vaibhav Raut</td>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+            <td>${empPayrollData._name}</td>
             <td>Male</td>
-            <td><div class="dept-label">Finance</div><div class="dept-label">Engineer</div></td>
-            <td>1000000</td>
-            <td>5 Feb 2024</td>
+            <td><div class="dept-label">${empPayrollData._department[0]}</div><div class="dept-label">${empPayrollData._department[1]}</div></td>
+            <td>${empPayrollData._salary} </td>
+            <td>${empPayrollData._startDate}</td>
             <td>
-                <img src="Media/delete-black-18dp.svg" alt="delete" id="1">
-                <img src="Media/create-black-18dp.svg" alt="edit" id="1">
+                <img name="${empPayrollData._id}" src="Media/delete-black-18dp.svg" alt="delete" id="1">
+                <img name="${empPayrollData._id}" src="Media/create-black-18dp.svg" alt="edit" id="1">
             </td>      
         </tr>
         `;
-    document.querySelector('#display').innerHTML = innerHtml;
+
+    }
+    document.querySelector('#display').innerHTML = innerHTML
 
 };
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Vaibhav Raut',
+            _gender: 'male',
+            _department: [
+                'Finance',
+                'Engineer'
+            ],
+            _salary: '1000000',
+            _startDate: '5 Feb 2024',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: 'Media/emp1.png'
+        },
+
+        {
+            _name: 'Pranav Raut',
+            _gender: 'male',
+            _department: [
+                'Sales',
+                'HR'
+            ],
+            _salary: '1000000',
+            _startDate: '1 Feb 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: 'Media/emp4.png'
+        }
+    ];
+
+    return empPayrollListLocal;
+}
